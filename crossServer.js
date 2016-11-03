5be3940da4e5116ae63fb6d4955b0174a97a7951
szarp <lizusfull@gmail.com>
2016-11-03 19:22:56 +0100

    change certificate location

--- crossServer.js
+++ crossServer.js
@@ -29,16 +29,17 @@ POST /{recipient_userid}/notifications?
      template=You have people waiting to play with you, play now!
      
 */
+
 var opts = {
    
   // Specify the key file for the server
-  key: fs.readFileSync('/Users/bartek/Documents/2016/wsskey.pem'),
+  key: fs.readFileSync('/home/madar/2016/wsskey.pem'),
    
   // Specify the certificate file
-  cert: fs.readFileSync('/Users/bartek/Documents/2016/wsscert.pem'),
+  cert: fs.readFileSync('/home/madar/2016/wsscert.pem'),
    
   // Specify the Certificate Authority certificate
-  ca: fs.readFileSync('/Users/bartek/Documents/2016/cacert.pem'),
+  ca: fs.readFileSync('/home/madar/2016/cacert.pem'),
    
   // This is where the magic happens in Node.  All previous
   // steps simply setup SSL (except the CA).  By requesting
