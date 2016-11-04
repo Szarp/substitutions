6f09a78249c99fa038763ae6aaf16e41b6859717
szarp <lizusfull@gmail.com>
2016-11-04 23:09:19 +0100

    first try with login

--- crossServer.js
+++ crossServer.js
@@ -144,7 +144,10 @@ app.post('/redirect', function(req, res){
 	//es.sendFile( __dirname + '/public/css/webPage.css');
 
 });
-app.get('/testSetCookie', function(req, resp){
+app.get('/testLogin', function(req, resp){
+    var login=x.loginLink()
+    
+    resp.redirect(login);
     //console.log('hi');
     //var a=new setDate();
     //a.todayIs();
@@ -155,7 +158,7 @@ app.get('/testSetCookie', function(req, resp){
     //console.log(a.z);
     //console.log(a.params);
     //asd();
-    resp.send('ok');
+    //resp.send('ok');
 	//es.sendFile( __dirname + '/public/css/webPage.css');
 
 });
@@ -213,6 +216,82 @@ myFunc.subs('2016-10-07',function(y){
 })
 */
 //myFunc.getCookie(function(){});//mongoTest();
+var x=new createReqest();
+
+
+app.get('/redirect_login', function(req, res){
+        console.log('login reqest');
+    console.log('query' ,req.query);
+    //console.log(req.params);
+    //console.log(req.cookies.cookieName);
+
+    //console.log(res.headers);
+    
+    res.send('ok');
+	//es.sendFile( __dirname + '/public/css/webPage.css');
+
+});
+
+
+function appSetting(){
+    var data=fs.readFileSync('/home/madar/2016/config.json')  
+    var parsed=JSON.parse(data);
+    //this.clientId=1082740245094082;
+    //this.redirectUri='https://muwbackup.emadar.eu:8088/redirect'
+    //this.oauth = 'https://www.facebook.com/v2.8/dialog/oauth?'
+    this.clientId=data['clientId'];
+    this.url=['redirectUrl'];
+    this.appSecret=['appSecret'];
+    
+    
+    
+}
+
+
+function createReqest (){
+    appSetting.call(this);
+    
+    this.loginLink=function(){
+        var redirect='/redirect_login';
+        return  this.oauth+'client_id='+this.clientId+'&redirect_url='+this.url+redirect;
+        /*
+        
+        https://www.facebook.com/v2.8/dialog/oauth?
+        client_id={app-id}
+        &redirect_uri={redirect-uri}
+        
+        */
+    }
+    this.codeForAcces=function(code){
+        var redirect='/redirect_codeAcces';
+        return 'https://graph.facebook.com/v2.8/oauth/access_token?'+'client_id='+this.clientId+'&redirect_uri='+this.url+redirect+'&code='+code;
+        
+       /*
+       
+           GET https://graph.facebook.com/v2.8/oauth/access_token?
+       client_id={app-id}
+       &redirect_uri={redirect-uri}
+       &client_secret={app-secret}
+       &code={code-parameter}
+       
+       */
+    }
+    this.appSecret=function(){
+        var redirect='/redirect_appSecret';
+        return 'https://graph.facebook.com/v2.8/oauth/access_token?client_id='+this.clientId+'&client_secret='+this.appSecret+'&grant_type=client_credentials';
+        
+        /*
+        
+        https://graph.facebook.com/v2.8/oauth/access_token
+        ?client_id={app-id}
+        &client_secret={app-secret}
+        &grant_type=client_credentials
+        
+        */
+    }
+}
+
+
 
 function mongoTest(){
         MongoClient.connect('mongodb://localhost:27017/test2', function(err, db) {
@@ -231,24 +310,3 @@ function mongoTest(){
     });
 
 }
-var exData =[
-    {"cancelled":[true],"note":[""],"substitution_types":[""],"type":["card"],"periods":["1"],"subjects":["angielski"],"teachers":["Ceglinska"],"classes":["2b"],"classrooms":["5"],"groupnames":["2. Grupa"],"periodorbreak":["00P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Ogrocka"],"subjects":["historia 5"]},"type":["card"],"periods":["3"],"subjects":["polski 5"],"teachers":["Stankowska"],"classes":["2a","2b","2c","2d"],"classrooms":["31"],"groupnames":["seminargroup:4"],"periodorbreak":["02P"],"moje":[false]},{"cancelled":[false],"substitution_types":["za III GD"],"note":[""],"changes":{"teachers":["Achtelik"],"subjects":["g.wych"]},"type":["card"],"periods":["3"],"subjects":["wos"],"teachers":["Antonowicz"],"classes":["2gd"],"classrooms":["13"],"groupnames":[""],"periodorbreak":["02P"],"moje":[false]},{"cancelled":[false],"substitution_types":["łączone"],"note":[""],"changes":{"teachers":["Taca Andrzej"]},"type":["card"],"periods":["4"],"subjects":["wf"],"teachers":["Głodny Szymon"],"classes":["1gc"],"classrooms":["0gim"],"groupnames":["Dziewczęta"],"periodorbreak":["03P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Ogrocka"]},"type":["card"],"periods":["4"],"subjects":["sih 3"],"teachers":["Glombik"],"classes":["2a","2d"],"classrooms":["5"],"groupnames":["seminargroup:3"],"periodorbreak":["03P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Jałowiecki"],"subjects":["matematyka"]},"type":["card"],"periods":["4"],"subjects":["biologia 4"],"teachers":["Kucharz"],"classes":["2b","2c"],"classrooms":["10"],"groupnames":["seminargroup:4"],"periodorbreak":["03P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Kruszelnicka"]},"type":["card"],"periods":["4"],"subjects":["angielski"],"teachers":["Pilch"],"classes":["3d"],"classrooms":["33"],"groupnames":["1. Grupa"],"periodorbreak":["03P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Pordzik"],"subjects":["g.wych"]},"type":["card"],"periods":["5"],"subjects":["polski"],"teachers":["Stankowska"],"classes":["1c"],"classrooms":["31"],"groupnames":[""],"periodorbreak":["04P"],"moje":[false]},{"cancelled":[false],"substitution_types":["łączone"],"note":[""],"changes":{"teachers":["Taca Andrzej"]},"type":["card"],"periods":["5"],"subjects":["wf"],"teachers":["Głodny Szymon"],"classes":["1gc"],"classrooms":["0gim"],"groupnames":["Dziewczęta"],"periodorbreak":["04P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Skoczylas Ewa"],"subjects":["niemiecki"]},"type":["card"],"periods":["5"],"subjects":["sih 8"],"teachers":["Glombik"],"classes":["2a","2b","2c","2d"],"classrooms":["32"],"groupnames":["seminargroup:1"],"periodorbreak":["04P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Wiencierz"],"subjects":["matematyka"]},"type":["card"],"periods":["5"],"subjects":["przyroda 5"],"teachers":["Kucharz"],"classes":["2a","2b","2c","2d"],"classrooms":["10"],"groupnames":["seminargroup:4"],"periodorbreak":["04P"],"moje":[false]},{"cancelled":[false],"substitution_types":["łączone"],"note":[""],"changes":{"teachers":["Mazur Iza"]},"type":["card"],"periods":["2"],"subjects":["wf"],"teachers":["Głodny Szymon"],"classes":["1b"],"classrooms":["0gim"],"groupnames":["Dziewczęta"],"periodorbreak":["01P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Marian Aleksandra"],"subjects":["chemia 3"]},"type":["card"],"periods":["5"],"subjects":["biologia 3"],"teachers":["Antonowicz"],"classes":["2a","2d"],"classrooms":["22"],"groupnames":["seminargroup:5"],"periodorbreak":["04P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Wlisłocka Małgorzata"],"subjects":["matematyka"]},"type":["card"],"periods":["5"],"subjects":["angielski"],"teachers":["Ceglinska"],"classes":["3a"],"classrooms":["5"],"groupnames":["1. Grupa"],"periodorbreak":["04P"],"moje":[false]},{"cancelled":[false],"substitution_types":["za III GD"],"note":[""],"changes":{"teachers":["Kaczmar"],"subjects":["niemiecki"]},"type":["card"],"periods":["5"],"subjects":["polski"],"teachers":["Żmuda Paweł"],"classes":["3d"],"classrooms":["3"],"groupnames":[""],"periodorbreak":["04P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Pordzik"],"subjects":["francuski"]},"type":["card"],"periods":["6"],"subjects":["angielski"],"teachers":["Ceglinska"],"classes":["1a","1c"],"classrooms":["5"],"groupnames":["seminargroup:2"],"periodorbreak":["05P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Niezgoda"],"subjects":["niemiecki"]},"type":["card"],"periods":["6"],"subjects":["angielski"],"teachers":["Pilch"],"classes":["1a","1c"],"classrooms":["19"],"groupnames":["seminargroup:4"],"periodorbreak":["05P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Adam"],"subjects":["chemia"]},"type":["card"],"periods":["6"],"subjects":["biologia"],"teachers":["Kucharz"],"classes":["1d"],"classrooms":["10"],"groupnames":[""],"periodorbreak":["05P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Cwołek"],"subjects":["matematyka"]},"type":["card"],"periods":["6"],"subjects":["polski"],"teachers":["Żmuda Paweł"],"classes":["2a"],"classrooms":["3"],"groupnames":[""],"periodorbreak":["05P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Jałowiecki"],"subjects":["matematyka"]},"type":["card"],"periods":["6"],"subjects":["g.wych"],"teachers":["Stankowska"],"classes":["2c"],"classrooms":["31"],"groupnames":[""],"periodorbreak":["05P"],"moje":[false]},{"cancelled":[false],"substitution_types":["za 9 lekcję"],"note":[""],"changes":{"teachers":["Burnus"],"subjects":["religia"],"classrooms":["14"]},"type":["card"],"periods":["6"],"subjects":["biologia"],"teachers":["Antonowicz"],"classes":["2gb"],"classrooms":["28"],"groupnames":[""],"periodorbreak":["05P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Wróbel"],"subjects":["informatyka"],"classrooms":["37"]},"type":["card"],"periods":["7"],"subjects":["historia"],"teachers":["Glombik"],"classes":["1d"],"classrooms":["10"],"groupnames":["2. Grupa"],"periodorbreak":["06P"],"moje":[false]},{"cancelled":[true],"note":[""],"substitution_types":[""],"type":["card"],"periods":["2"],"subjects":["biologia"],"teachers":["Antonowicz"],"classes":["1gb"],"classrooms":["3"],"groupnames":["1. Grupa"],"periodorbreak":["01P"],"moje":[false]},{"cancelled":[false],"substitution_types":["łączone"],"note":[""],"changes":{"teachers":["Lee"],"classrooms":["5"]},"type":["card"],"periods":["7"],"subjects":["angielski"],"teachers":["Pilch"],"classes":["2gb"],"classrooms":["19"],"groupnames":["2. Grupa"],"periodorbreak":["06P"],"moje":[false]},{"cancelled":[true],"note":[""],"substitution_types":[""],"type":["card"],"periods":["7"],"subjects":["angielski"],"teachers":["Ceglinska"],"classes":["3ga"],"classrooms":["5"],"groupnames":["2. Grupa"],"periodorbreak":["06P"],"moje":[false]},{"cancelled":[true],"note":[""],"substitution_types":[""],"type":["card"],"periods":["8"],"subjects":["historia"],"teachers":["Glombik"],"classes":["1d"],"classrooms":["10"],"groupnames":["1. Grupa"],"periodorbreak":["07P"],"moje":[false]},{"cancelled":[true],"note":[""],"substitution_types":[""],"type":["card"],"periods":["8"],"subjects":["angielski"],"teachers":["Ceglinska"],"classes":["3b"],"classrooms":["5"],"groupnames":["1. Grupa"],"periodorbreak":["07P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Kruszelnicka"]},"type":["card"],"periods":["8"],"subjects":["angielski"],"teachers":["Pilch"],"classes":["3c"],"classrooms":["49"],"groupnames":["2. Grupa"],"periodorbreak":["07P"],"moje":[false]},{"cancelled":[true],"note":[""],"substitution_types":[""],"type":["card"],"periods":["8"],"subjects":["wf"],"teachers":["Głodny Szymon"],"classes":["3gc"],"classrooms":["0gim"],"groupnames":["Dziewczęta"],"periodorbreak":["07P"],"moje":[false]},{"cancelled":[true],"note":[""],"substitution_types":[""],"type":["card"],"periods":["9"],"subjects":["wf"],"teachers":["Głodny Szymon"],"classes":["3gc"],"classrooms":["0gim"],"groupnames":["Dziewczęta"],"periodorbreak":["08P"],"moje":[false]},{"cancelled":[true],"substitution_types":[""],"type":["card"],"periods":["4"],"subjects":["niemiecki"],"teachers":["Zajdel"],"classes":["3gd"],"classrooms":["32"],"periodorbreak":["03P"],"moje":[false]},{"cancelled":[true],"substitution_types":[""],"type":["card"],"periods":["8"],"subjects":["matematyka"],"teachers":["Drohojowski"],"classes":["3gd"],"classrooms":["4"],"periodorbreak":["07P"],"moje":[false]},{"cancelled":[true],"substitution_types":[""],"type":["card"],"periods":["7"],"subjects":["religia"],"teachers":["Burnus"],"classes":["3gd"],"classrooms":["14"],"periodorbreak":["06P"],"moje":[false]},{"cancelled":[true],"note":[""],"substitution_types":[""],"type":["card"],"periods":["2"],"subjects":["biologia"],"teachers":["Kucharz"],"classes":["1gd"],"classrooms":["10"],"groupnames":["2. Grupa"],"periodorbreak":["01P"],"moje":[false]},{"cancelled":[true],"substitution_types":[""],"type":["card"],"periods":["6"],"subjects":["religia"],"teachers":["Burnus"],"classes":["3gd"],"classrooms":["14"],"periodorbreak":["05P"],"moje":[false]},{"cancelled":[true],"substitution_types":[""],"type":["card"],"periods":["4"],"subjects":["niemiecki"],"teachers":["Kaczmar"],"classes":["3gd"],"classrooms":["7"],"periodorbreak":["03P"],"moje":[false]},
-    {"cancelled":[true],"substitution_types":[""],"type":["card"],"periods":["3"],"subjects":["zajęcia techniczne"],"teachers":["Achtelik"],"classes":["3gd"],"classrooms":["34"],"periodorbreak":["02P"],"moje":[false]},{"cancelled":[false],"changes":{},"type":["card"],"periods":["5"],"subjects":["zaj art"],"teachers":["Kupiec"],"classes":["3gc","3gd"],"classrooms":["42"],"periodorbreak":["04P"],"moje":[false]},{"cancelled":[false],"changes":{},"type":["card"],"periods":["5"],"subjects":["zaj art"],"teachers":["Warzecha"],"classes":["3gc","3gd"],"classrooms":["47"],"periodorbreak":["04P"],"moje":[false]},{"cancelled":[true],"substitution_types":[""],"type":["card"],"periods":["3"],"subjects":["niemiecki"],"teachers":["Kaczmar"],"classes":["3gd"],"classrooms":["7"],"periodorbreak":["02P"],"moje":[false]},{"cancelled":[true],"substitution_types":[""],"type":["card"],"periods":["2"],"subjects":["polski"],"teachers":["Stankowska"],"classes":["3gd"],"classrooms":["31"],"periodorbreak":["01P"],"moje":[false]},{"cancelled":[false],"substitution_types":["łączone"],"note":[""],"changes":{"teachers":["Aureliusz Wieczorek"],"classrooms":["10"]},"type":["card"],"periods":["2"],"subjects":["angielski"],"teachers":["Ceglinska"],"classes":["2b"],"classrooms":["5"],"groupnames":["1. Grupa"],"periodorbreak":["01P"],"moje":[false]},{"cancelled":[true],"note":[""],"substitution_types":[""],"type":["card"],"periods":["3"],"subjects":["polski"],"teachers":["Żmuda Paweł"],"classes":["1d"],"classrooms":["36"],"groupnames":[""],"periodorbreak":["02P"],"moje":[false]},{"cancelled":[false],"substitution_types":["płatne"],"note":[""],"changes":{"teachers":["Błaszczykowska"],"subjects":["biologia"],"classrooms":["33"]},"type":["card"],"periods":["3"],"subjects":["angielski"],"teachers":["Ceglinska"],"classes":["1ga"],"classrooms":["5"],"groupnames":["1. Grupa"],"periodorbreak":["02P"],"moje":[false]},{"cancelled":[false],"substitution_types":["łączone"],"note":[""],"changes":{"teachers":["Błaszczykowska"],"subjects":["biologia"]},"type":["card"],"periods":["3"],"subjects":["angielski"],"teachers":["Pilch"],"classes":["1ga"],"classrooms":["33"],"groupnames":["2. Grupa"],"periodorbreak":["02P"],"moje":[false]},{"cancelled":[false],"substitution_types":["za III GD"],"note":[""],"changes":{"teachers":["Kaczmar"],"subjects":["niemiecki"],"classrooms":["7"]},"type":["card"],"periods":["3"],"subjects":["biologia"],"teachers":["Kucharz"],"classes":["1gd"],"classrooms":["10"],"groupnames":["1. Grupa"],"periodorbreak":["02P"],"moje":[false]}]
-
-        
-        
-        
-    
-
-
-   
-
-
-
-
-
-
-
-
-
-
