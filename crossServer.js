6189b0a66ca5a81bc371145ca3d8179ade54f62b
szarp <lizusfull@gmail.com>
2016-11-04 23:27:55 +0100

    n

--- crossServer.js
+++ crossServer.js
@@ -236,7 +236,7 @@ app.get('/redirect_login', function(req, res){
 
 function appSetting(){
     var data=fs.readFileSync('/home/madar/2016/appConfig.json');
-    console.log(JSON.parse(data));
+    console.log('parese', JSON.parse(data));
     var parsed=JSON.parse(data);
     //this.clientId=1082740245094082;
     //this.redirectUri='https://muwbackup.emadar.eu:8088/redirect'
