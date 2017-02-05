16661249caeb0e1b53c62318ae03dc75f02985c3
krzysdz <krzysdz@konto.pl>
2017-02-05 18:07:14 +0100

    require request

--- myModules/messengerBot.js
+++ myModules/messengerBot.js
@@ -1,5 +1,6 @@
 var config = require('./config');
 var manageUsers = require('./manageUsers.js');
+var request = require('request');
 //var mongo = require('./mongoFunctions.js');
 
 function sendSubstitutions(senderID){
