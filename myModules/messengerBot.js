c3d718e0f3910c90cf563c44c22ac07441ec5bb2
krzysdz <krzysdz@konto.pl>
2017-02-06 17:57:14 +0100

    Fix crash on attachments

--- myModules/messengerBot.js
+++ myModules/messengerBot.js
@@ -137,54 +137,50 @@ function sendSubstitutions(senderID, message){
 }
 
 function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
+	var senderID = event.sender.id;
+	var recipientID = event.recipient.id;
+	var timeOfPostback = event.timestamp;
 
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
-  var payload = event.postback.payload;
+	// The 'payload' param is a developer-defined field which is set in a postback 
+	// button for Structured Messages. 
+	var payload = event.postback.payload;
 
-  console.log("Received postback for user %d and page %d with payload '%s' " + 
-    "at %d", senderID, recipientID, payload, timeOfPostback);
+	console.log("Received postback for user %d and page %d with payload '%s' " + 
+	"at %d", senderID, recipientID, payload, timeOfPostback);
 
-  // When a postback is called, we'll send a message back to the sender to 
-  // let them know it was successful
-  //sendTextMessage(senderID, "Postback called");
+	// When a postback is called, we'll send a message back to the sender to 
+	// let them know it was successful
+	//sendTextMessage(senderID, "Postback called");
 }
 
 function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
+	var senderID = event.sender.id;
+	var recipientID = event.recipient.id;
+	var timeOfMessage = event.timestamp;
+	var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
+	console.log("Received message for user %d and page %d at %d with message:", 
+	senderID, recipientID, timeOfMessage);
+	console.log(JSON.stringify(message));
 
-  var messageId = message.mid;
+	var messageId = message.mid;
 
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
+	var messageText = message.text;
+	var messageAttachments = message.attachments;
+	var help = 'NIE PRZYJMUJEMY ZAŁĄCZNIKÓW\nDostępne polecenia to:\n"0 <klasa>" - zastępstwa dla klasy na dzisiaj\n"1 <klasa>" - zastępstwa dla klasy na jutro\n"2 <pytanie>" - pomoc';
 
-  /*if (messageText) {
-
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }*/
-  sendSubstitutions(senderID, messageText);
-  //sendTextMessage(senderID, messageText);
+	if (messageText) {
+		sendSubstitutions(senderID, messageText);
+	} else {
+		var messageData = {
+			recipient: {
+			  id: senderID
+			},
+			message: {
+			  text: help
+			}
+		};
+	}
 }
 
 function callSendAPI(messageData) {
