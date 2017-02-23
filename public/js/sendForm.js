9d22d989b4f4d6abbb580045952a735f1146a3cb
krzysdz <krzysdz@konto.pl>
2017-02-23 16:17:24 +0100

    Fix path not changed in script

--- public/js/sendForm.js
+++ public/js/sendForm.js
@@ -167,7 +167,7 @@ function onLoadFunc(){
     requestForChanges('today'); 
     });
 	if('serviceWorker' in navigator){
-		navigator.serviceWorker.register('/js/service-worker.js', {
+		navigator.serviceWorker.register('/service-worker.js', {
 			scope: './'
 		});
     }
