320f6c4ab4f3c09d70b56d133161940e397adbc8
krzysdz <krzysdz@konto.pl>
2017-05-08 09:36:47 +0200

    Fix bugs made in cc34c20

--- myModules/zsoServerComunication.js
+++ myModules/zsoServerComunication.js
@@ -58,7 +58,7 @@ var getSomeSubstitution = function(date,callback){
 						var beforeTe = obj.teachers;
 						var newTe = beforeTe.concat(teachers);
 						newTe = uniq(newTe);
-						if(newTe.prototype.equals(beforeTe)){
+						if(!arrayEqual(newTe, beforeTe)){
 							mongo.modifyById('all', 'teachers', {'teachers': teachers}, function(){
 								console.log('Teachers list updated');
 							})
@@ -295,41 +295,17 @@ function getGPIDandGSH (data,callback){ //take params to send request
 	});
 }
 
-/*
-	define Array.prototype.equals - campares arrays and returns true if they are the same
-	doesn't work with objects as elements of an array (2 same objects always are different)
-	source: http://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript/14853974#14853974
-*/
-
-// Warn if overriding existing method
-if(Array.prototype.equals)
-	console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
-// attach the .equals method to Array's prototype to call it on any array
-Array.prototype.equals = function (array) {
-	// if the other array is a falsy value, return
-	if (!array)
+function arrayEqual(array0, array){
+	if (array0.length != array.length){
 		return false;
-
-	// compare lengths - can save a lot of time
-	if (this.length != array.length)
-		return false;
-
-	for (var i = 0, l=this.length; i < l; i++) {
-		// Check if we have nested arrays
-		if (this[i] instanceof Array && array[i] instanceof Array) {
-			// recurse into the nested arrays
-			if (!this[i].equals(array[i]))
-				return false;
-		}
-		else if (this[i] != array[i]) {
-			// Warning - two different object instances will never be equal: {x:20} != {x:20}
+	}
+	for (var i = 0; i < array0.length; i++) {
+		if (array0[i] != array[i]) {
 			return false;
 		}
 	}
 	return true;
 }
-// Hide method from for-in loops
-Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
 exports.subs = getSomeSubstitution;
 exports.getData = getData;
