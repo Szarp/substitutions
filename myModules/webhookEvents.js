111bfd7481eda864420e412008366edf9af6a43e
szarp <lizusfull@gmail.com>
2017-10-14 16:01:04 +0200

    define message if is response from server, postback, plain text message or message with attachments. Then connect with approach actions

--- myModules/webhookEvents.js
+++ myModules/webhookEvents.js
@@ -1,9 +1,109 @@
 var request=require('request');
 var config = require('./config');
+var splitText = require('./splitText.js');
+var mess = require('./messTemplates.js');
+var mongo =require('./mongoFunctions.js');
 
 /*
 webhookEvents
 */
+//message text true false 
+//is_echo: true false
+//attachments: true false
+//postcall true fasle
+//delivery true false
+function saveToUserBuff(id,mid,timestamp){
+    var obj= {
+        system:{
+            messages:[true]
+        
+        }
+        }
+    mongo.findByParam({"personal.id":id},{_id:true},function(list){
+        console.log(list);
+    })
+    mongo.modifyById(id,'person',obj,function(){
+        console.log('ok');
+        
+        
+    })
+}
+function messageDistribution(mess){
+    switch(mess.type){
+        case "read":
+            console.log("Clearing messages...");
+            //clear user buff
+        break;
+        case "delivery":
+            //do nothing but do not let anything happen
+        break;
+        case "postback":
+            if(mess["echo"] != true){
+                //postback function
+                console.log("That is clicked postback");
+                console.log('Saving to user\'s message');
+            }
+        break;
+        default:
+            if(mess["echo"]==true){
+                //console.log('Saving to user message');
+                console.log('Saving to server\'s message');
+                
+            }
+            else{
+                console.log('check if attachments or text');
+                console.log('Saving to users\'s message');
+            }
+        break;
+            
+            //do something else    
+    }
+    
+    
+}
+function messageLogistic(params,event){
+    var mess={}
+    mess["timestamp"]=event.timestamp;
+    mess["sender"]=event.sender.id;
+    mess["page"]=event.recipient.id;
+    mess["type"]="";
+    console.log('begin');
+    //switch(true){
+        if(params.attachments == true){
+            mess["attachments"]=event.message.attachments;
+            //console.log('    got attachments')
+        }
+        if(params.text == true){
+            mess["text"] = event.message.text;
+            //console.log('    text type')
+        }
+        if(params.read == true){
+            mess["type"]="read";
+            mess["watermark"]=event.read.watermark;
+            //console.log('    user read message')
+        }
+        if(params.payload == true){
+            mess["type"]="postback";
+            mess["payload"]=event.postback.payload;
+            //console.log('    postback mess');
+        }
+        if(params.delivery == true){
+            mess["type"]="delivery";
+            mess["watermark"]=event.delivery.watermark;
+            //console.log('   deliverd message');
+        }
+        if(params.echo == true){
+            mess["echo"]=true;
+            //console.log('   message from server')
+        }
+        //break;
+    //}
+    messageDistribution(mess);
+    //console.log(mess);
+    console.log('end;');
+}
+
+
 function reduceElements(type,event){
     //type delivery, message
     var pattern={
@@ -28,6 +128,7 @@ function reduceElements(type,event){
             pattern.watermark=event[type].watermark;
             pattern["text"]=event.message["text"];
              pattern["mid"]=event[type].mid;
+            pattern["attachments"]=event.message["attachments"];
         }
     }
     else if(type=="delivery"){
@@ -37,8 +138,18 @@ function reduceElements(type,event){
    return pattern;
     
 }
+function attachments(event){
+    var type = event.message.attachment.type;
+    var link = event.message.attachment.payload.url;
+    console.log('Got attachments: '+type)
+    console.log('Location: '+link)
+    
+}
 function delivered(event){
-    //console.log('delivered',event);
+    //var mid = event.delivery.mids.mid;
+    var id = event.sender.id;
+    var time = event.watermark;
+    console.log('All '+id+'\'s messages habe been senn before '+time);
     //console.log('delivery',reduceElements("delivery",event));
 }
 function echo(event){
@@ -57,16 +168,78 @@ function echo(event){
      seq: 492132,
      attachments: [ [Object] ] } }
     */
-    
+}
+function createButtons(tab, callback){
+	var buttons=[];
+	for(var i = 0; i < tab.length; i++){
+		var btn = tab[i];
+		var singleBTN={};
+		singleBTN['type']=btn[0];
+		singleBTN['title']=btn[2];
+		if(btn[0]=='web_url'){
+			singleBTN['url']=btn[1];
+		} else if(btn[0]=='postback'){
+			singleBTN['payload']=btn[1];
+		}
+		console.log(singleBTN);
+		console.log(JSON.stringify(singleBTN));
+		buttons.push(singleBTN);
+	}
+	setImmediate(function(){
+		console.log(buttons);
+		callback(buttons);
+	});
+}
+
+function createMessage(type, id, content, callback){
+	var message = {
+		recipient: {
+		  id: id
+		}
+	};
+	console.log(content);
+	if(type == 'text'){
+		message['message']={text: content};
+	}else{
+		message['message']={
+			attachment:{
+				type: 'template',
+				payload:{
+					template_type: 'button',
+					text: content['text'],
+					buttons: content['buttons']
+				}
+			}
+		}
+	}
+	setImmediate(function(){
+		callback(message);
+	});
 }
 function message(event) {
     //console.log('mes',event);
     console.log('mes',reduceElements("message",event));
     var mes = reduceElements("message",event);
+    console.log(mes.attachments);
     //console.log('echo',reduceElements("message",event));
-    createMessage('text', mes.sender, "Nie potrafimy odpowiedzieć na pytanie, którego nie zadano.\nPrzykro nam :'(", function(messageTS){
+    //splitText.split(mes.text);
+    //console.log(helpPageMessage(mes.sender));
+    //callSendAPI(mess.unreadMessage(mes.sender));
+    //saveToUserBuff(mes.sender);
+    createButtons([['postback', 'example', 'Przykład']], function(buttons){
+				var content={
+					'text': "hello",
+					'buttons': buttons
+				}
+				createMessage('generic', mes.sender, content, function(messageTS){
 					callSendAPI(messageTS);
-    });
+                    //callSendAPI(mess.helpPage(mes.sender));
+				});
+			});
+    //callSendAPI(mess.helpPage(mes.sender));
+    /*createMessage('text', mes.sender, "Nie potrafimy odpowiedzieć na pytanie, którego nie zadano.\nPrzykro nam :'(", function(messageTS){
+					callSendAPI(messageTS);
+    });*/
     //console.log('ok');
     /*
     mes { sender: { id: '1383716548353914' },
@@ -168,13 +341,14 @@ function callSendAPI(messageData) {
         messageId, recipientId);
     } else {
       console.error("Unable to send message.");
-      //console.error(response);
+      console.error(response.body);
       console.error(error);
     }
   });
 }
-
 exports.delivered=delivered;
 exports.echo=echo;
 exports.postback=postback;
 exports.message=message;
+exports.attachments=attachments;
+exports.messageLogistic=messageLogistic;
