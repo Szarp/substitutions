93ed8bd15f9037a58c299f4eca9a6f4c9c966798
szarp <lizusfull@gmail.com>
2018-02-28 23:00:37 +0100

    module to download changes for all pages

---
+++ myModules/downloadChanges.js
@@ -0,0 +1,26 @@
+//downloadChanges.js
+var zckoiz = require("./zckioz_mesLogistic.js");
+var zso11 = require("./zso11_mesLogistic.js");
+var setTime = require('./setTime.js');
+var time = new setTime();
+function downloadAll(){
+    getZckioz();
+    getZso11();
+    
+}
+function getZso11(){
+    time.tommorowIs();
+    zso11.subs(time.displayTime(),function(y){
+        time.todayIs();
+        zso11.subs(time.displayTime(),function(b){
+            time.theDayAfterTomorrowIs();
+            zso11.subs(time.displayTime(),function(x){
+		          console.log('downloaded changes zso11');
+            });
+        });
+    });
+}
+function getZckioz(){
+    zckoiz.subs(function(){console.log('downloaded changes zckoiz');});
+}
+exports.all=downloadAll;
