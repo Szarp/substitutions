7d0edc860aacc8e51b5dd08d5fba27aad26a19d7
szarp <lizusfull@gmail.com>
2018-03-26 23:01:00 +0200

    func status and info for madar communication

--- myModules/madar_mesLogistic.js
+++ myModules/madar_mesLogistic.js
@@ -1,38 +1,230 @@
+var request=require('request'),
+    querystring = require('querystring');
+var config = require('./configs/madar');
+var mon = require('./mongoConnection.js');
+//messenger
+var template = require('./messTemplates.js');
+//var mess = require('./messTemplates.js');
+var messFunc = require('./messFunctions.js');
+var secretToken = require('./secretTokenGenerator.js');
+var setTime = require('./setTime.js');
+var time = new setTime();
+/*
+webhookEvents
+*/
+var serverDB = new mon.server(config.db);
+var userDB = new mon.user(config.db);
+var subDB = new mon.substitutions(config.db);
+var messenger = new messFunc.send(config.pageToken);
 Date.prototype.localTime = function(){
    return this.getTime() - this.getTimezoneOffset()*1000*60/2;
 }
+//serverDB.init();
+//subDB.collectionList(function(e,r){console.log(r)});
+//subDB.remove("2017-12-14",function(e,r){console.log(r.result)});
+//insertRandomMessages(10000);
+
+//userDB.init();
+//userDB.remove("userMessages",function(e,r){console.log(e,r.result)});
+//subDB.find({},{},function(e,r){console.log(e,r)});
+
 var madar = new messengerFunc(config.madarParams);
-    madar.status(function(status){
-        console.log("status")
-        for(k in status)
-            console.log(k+" devices: "+status[k]);
-    });
-    //z.sensor(,function(a){});
-madar.sensor("001703e1",function(res){console.log("sensor",res)});
-//z.lastMeasure("001703e1",function(res){console.log("measure",res)});
-madar.sensor("0017008f",function(res){console.log("sensors",res)});
-//madar.status(function(res){console.log("sensor",res)});
+function isThisMe(pageId){
+    return (pageId==config.pageId);
+}
+function messageDistribution(mess){
+    console.log("mess",mess);
+    switch(mess.type){
+        case "read":
+            //console.log("Clearing messages...");
+            //clear user buff
+        break;
+        case "delivery":
+            //do nothing but do not let anything happen
+        break;
+        case "postback":
+            if(mess["echo"] != true){
+                //postback function
+                console.log("postback");
+                
+                serverDB.save(mess,function(e,r){
+                     if(!e){
+                        console.log('Saving  users\'s message',r["result"]);
+                         if(r["result"]["nModified"]!=0){
+                            analizePostback(mess); 
+                         }
+                    }
+                    else{
+                        console.log("Error in saving users\'s message",e);
+                    }
+                });
+            }
+        break;
+        default:
+            if(mess["echo"]==true){
+                //console.log('Saving to user message');
+                serverDB.save(mess,function(e,r){
+                    if(!e){
+                        console.log('Saving  server\'s message',r["result"]);    
+                    }
+                    else{
+                        console.log("Error in saving server\'s message",e);
+                    }
+                });
+            }
+            else{
+                userDB.save(mess,function(e,r){
+                    if(!e){
+                        if(!mess["attachments"]){
+                            if(r["result"]["nModified"]!=0){
+                                analizeText(mess);
+                            }
+                    }
+                    else{
+                        //analizeAttachments(mess);
+                        //console.log("atta",mess);
+                    }
+                            console.log('Saving users\'s message',r["result"]);    
+                        }
+                        else{
+                            console.log("Error in saving user\'s message",e);
+                        }
+                })
+                //console.log('Saving to users\'s message');
+            }
+        break;
+    }
+}
+function token(text,mess){
+    	day='';
+    var senderID=mess.sender;
+    var tkn = text[1];
+		if(!tkn){
+            secretToken.messRequest(senderID, function(token){
+				var txt = 'Wygenerowany token wipsz na domek.emadar.eu po zalogowaniu i kliknięciu właego zdjęcia profilowego w polu "Sprawdź token"\nTwój token to: ' + token;
+					messFunc.preapreMessage('text', senderID, txt, function(messageTS){
+						messenger.send(messageTS);
+					});
+				});
+			} else {
+				tkn = parseInt(tkn);
+				console.log("Token received: " + tkn);
+				secretToken.messCheck(senderID, tkn, function(res){
+					if(res){
+						messFunc.preapreMessage('text', senderID, 'Konto zostało połączone. (y)', function(messageTS){
+							messenger.send(messageTS);
+						});
+					} else {
+						messFunc.preapreMessage('text', senderID, 'Wystąpił błąd. Spróbuj jeszcze raz.', function(messageTS){
+							send(messageTS);
+						});
+					}
+				});
+			}    
+}
+function analizeText(mess){
+    mess.text=mess.text.toLowerCase();
+    var text = mess.text.split(' ');
+    if(text[0]=="4"){
+            token(text,mess);        
+        }
+    else if(text.length == 2){
+        console.log("Maybe thats ask for changes");
+        if(text[0]=="info"){
+            madar.sensor(text[1],function(res){
+                var text=jsonToMessage(res);
+                messFunc.preapreMessage('text', mess.sender, text, function (messageTS) {
+                    messenger.send(messageTS);
+                });
+                console.log("res",res);
+            })
+        }
+    }
+    else{
+        if(text[0]=="pomoc"||text[0]=="help"){
+            console.log("user id",mess.sender);
+            messenger.send(template.helpPage(mess.sender));
+        }
+        else if(text[0]=="status"){
+            console.log("status");
+            madar.status(function(res){
+                console.log("res",res);
+                var text=jsonToMessage(res);
+                messFunc.preapreMessage('text', mess.sender, text, function (messageTS) {
+                    messenger.send(messageTS);
+                });
+            })
+            //console.log("user id",mess.sender);
+            //messenger.send(template.helpPage(mess.sender));
+        }
+        else{
+            console.log("Pop info about bad message to Admins");
+        }
+    }
+}
+function jsonToMessage(json){
+    var text="";
+    for(k in json){
+        text+=k+" : "+json[k]+"\n";
+    }
+    return text;
+}
+function analizePostback(mess) {
+    var payload = JSON.parse(mess.payload);
+    switch(payload.type){
+        case "example":
+		messFunc.preapreMessage('text', mess.sender, 'Chcę sprawdzić zastępstwa na dzisaj dla klasy 1b:\n0 1b', function(messageTS){
+			messenger.send(messageTS);
+        });
+        break;
+        default:
+        break;
+    }
+}
+function attachments(event){
+    var type = event.message.attachment.type;
+    var link = event.message.attachment.payload.url;
+    console.log('Got attachments: '+type)
+    console.log('Location: '+link)
+    
+}
+function delivered(event){
+    //var mid = event.delivery.mids.mid;
+    var id = event.sender.id;
+    var time = event.watermark;
+    console.log('All '+id+'\'s messages have been senn before '+time);
+    //console.log('delivery',reduceElements("delivery",event));
+}
+/* -----------------------------------------------------*/
+/* -----------------------------------------------------*/
+/* -----------------------------------------------------*/
+/* -----------------------------------------------------*/
+/* -----------------------------------------------------*/
 function messengerFunc(configParams){
     var self=this
     sensorStatus.call(this,configParams);
     //this.test=function(){console.log("test")};
     this.status=function(callback){
         self.macList(function(list){
-            list=JSON.parse(list)["connection"];
-            var connected=0;
-            var deconnected=-1;
-            for(k in list){
-                if(list[k]["RSSI"]!="")
-                    connected++;
-                else
-                    deconnected++;
+            if(list!= undefined){
+                list=JSON.parse(list)["connection"];
+                var connected=0;
+                var disconnected=-1;
+                for(k in list){
+                    if(list[k]["RSSI"]!="")
+                        connected++;
+                    else
+                        disconnected++;
+                }
+                setImmediate(function(){
+                    callback({"connected":connected,"disconnected":disconnected});
+                });
+            }else{
+                setImmediate(function(){
+                    callback({});
+                });
             }
-            //list.forEach(function(a){console.log("name",a.name,"RSSI",a.RSSI)})
-            //console.log(connected+" connected devices")
-            //console.log(deconnected+" deconnected devices")
-            setImmediate(function(){
-                callback({"connected":connected,"deconnected":deconnected});
-            });  
+                
         })
     }
 }
@@ -62,7 +254,7 @@ function sensorStatus(configParams){
                     })
                 }
                 else{
-                    form["status"]="deconnected";
+                    form["status"]="disconnected";
                     setImmediate(function(){
                         callback(form);
                     });
@@ -77,7 +269,7 @@ function sensorStatus(configParams){
     }
     this.convertName=function(text,callback){
         self.macList(function(list){
-            list=JSON.parse(list)["connection"]
+            list=JSON.parse(list)["connection"];
             text=text.toLocaleLowerCase();
             var sensor;
             for(k in list){
@@ -92,18 +284,30 @@ function sensorStatus(configParams){
         })
     }
     this.lastMeasure=function(macAddr,callback){
-        var x= new Date().localTime();
-        self.measure(macAddr,x-60*1000*20,x-60*1000*5,function(params){
+        var t= new Date().localTime();
+        self.measure(macAddr,t-60*1000*20,t-60*1000*5,function(params){
             setImmediate(function(){
                 callback(JSON.parse(params));
             });  
         });
     }
 }
+/*
+var x = new Madar();
+var time = new Date().localTime();
+x.values("00280007",time-60*1000*8,time,function(el){
+    el=JSON.parse(el)
+    console.log(el);
+    el=el.values;
+    el.forEach(function(elem){
+        console.log(elem.data);
+    })
+    //console.log("val",el);
+})*/
+
 function Madar(configParams){
     var self=this;
     this.dest="json.htm";
-    //this.name='bartek';
     this.name=configParams["name"];
     this.pass=configParams["pass"];
     this.url=configParams["url"];
@@ -150,7 +354,6 @@ function Madar(configParams){
         form["fromtime"]=ftime;
         form["tilltime"]=ttime;
         form["mac"]=macAddr;
-        //console.log("hi",form);
         self.request(form,function(res){
             setImmediate(function(){
                 callback(res);
@@ -163,7 +366,6 @@ function Madar(configParams){
         form["fromtime"]=ftime;
         form["tilltime"]=ttime;
         form["mac"]=macAddr;
-        //console.log("hi",form);
         self.request(form,function(res){
             setImmediate(function(){
                 callback(res);
@@ -224,3 +426,9 @@ function test(callback){
 				});
 			});
 }
+exports.delivered=delivered;
+//exports.postback=postback;
+//exports.message=message;
+exports.attachments=attachments;
+exports.messageDistribution=messageDistribution;
+exports.checkId=isThisMe;
